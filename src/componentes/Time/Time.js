bb217b555c8c6899f95c8599aaceb1916fe890be
luiz pellanda <luiz_pellanda@hotmail.com>
2023-02-23 17:04:19 -0300

    Refatoracao do componente ListaSuspensa

--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -3,7 +3,8 @@ import './Time.css';
 const Time = (props) => {
 
     const background = { backgroundColor: props.corSecundaria };
-    const sublinhado = { borderColor : props.corPrimaria }
+    const sublinhado = { borderColor : props.corPrimaria };
+    
     return (
         <section className='time' style={background}>
             <h3 style={sublinhado}>{props.nome}</h3>
