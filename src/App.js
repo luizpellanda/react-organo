07dedd210d89098839332839ff27929c04676a4f
luiz pellanda <luiz_pellanda@hotmail.com>
2023-02-23 14:52:33 -0300

    Componente Time criado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario/Formulario';
+import Time from './componentes/Time/Time';
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome='Programação' />
+      <Time nome='Frontend' />
+      <Time nome='Data Science' />
     </div>
   );
 }
