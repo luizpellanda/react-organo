7f737a4b290f42758653d35bb9f9bd1e421820e2
luiz pellanda <luiz_pellanda@hotmail.com>
2023-03-02 19:38:48 -0300

    Funcionalidade de alterar a cor dos componentes Time e Colaborador de forma dinamica

--- src/App.js
+++ src/App.js
@@ -3,46 +3,47 @@ import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario/Formulario';
 import Rodape from './componentes/Rodape/Rodape';
 import Time from './componentes/Time/Time';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState([
     {
       nome: 'Programação',
-      corPrimaria: '#57c278',
-      corSecundaria: '#D9F7E9'
+      cor: '#57c278',
+      id: uuidv4()
     },
     {
       nome: 'Front-End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      cor: '#82CFFA',
+      id: uuidv4()
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      cor: '#A6D157',
+      id: uuidv4()
     },
     {
       nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      cor: '#E06B69',
+      id: uuidv4()
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
+      cor: '#DB6EBF',
+      id: uuidv4()
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
+      cor: '#FFBA05',
+      id: uuidv4()
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#FF8A29',
+      id: uuidv4()
     }
-  ]
+  ])
 
   const [colaboradores, setColaboradores] = useState([]);
 
@@ -51,8 +52,20 @@ function App() {
     setColaboradores([...colaboradores, colaborador])
   }
 
-  function deletarColaborador () {
-    console.log('deletar')
+  function deletarColaborador (evento) {
+    evento.target.parentElement.remove();
+    if (colaboradores.length < 0) {
+      <Time />
+    }
+  }
+
+  function mudarCorDoTime(cor, id) {
+    setTimes(times.map(time => {
+      if(time.id === id) {
+        time.cor = cor
+      }
+      return time;
+    }))
   }
 
   return (
@@ -61,10 +74,11 @@ function App() {
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
       {times.map((time) => {
         return <Time 
+          id={time.id}
+          mudarCor={mudarCorDoTime}
           key={time.nome} 
           nome={time.nome} 
-          corPrimaria={time.corPrimaria} 
-          corSecundaria={time.corSecundaria} 
+          cor={time.cor}  
           colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
           aoDeletar={deletarColaborador}
         />
--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -1,28 +1,23 @@
 import Colaborador from '../Colaborador/Colaborador';
 import './Time.css';
+import hexToRgba from 'hex-to-rgba';
 
 const Time = (props) => {
 
-    const background = { backgroundColor: props.corSecundaria };
-    const sublinhado = { borderColor : props.corPrimaria };
+    const background = { backgroundColor: hexToRgba(props.cor, '0.3'), backgroundImage: 'url(/imagens/fundo.png)' };
+    const sublinhado = { borderColor : props.cor };
     
     return (
         //props.colaboradores.length > 0 && - maneira do REACT fazer renderizacao condicional
-        (props.colaboradores.length > 0) && <section className='time' style={background}>
+        (props.colaboradores.length > 0) && 
+        
+        <section className='time' style={background}>
+            <input type='color' className='input-cor' onChange={evento => props.mudarCor(evento.target.value, props.id)}/>
             <h3 style={sublinhado}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map( colaborador => {
-
-                    console.log(<Colaborador 
-                        corDeFundo={props.corPrimaria} 
-                        nome={colaborador.nome} 
-                        cargo={colaborador.cargo} 
-                        imagem={colaborador.imagem} 
-                        key={colaborador.nome}
-                        aoDeletar={props.aoDeletar} 
-                    />)
                     return <Colaborador 
-                        corDeFundo={props.corPrimaria} 
+                        corDeFundo={props.cor} 
                         nome={colaborador.nome} 
                         cargo={colaborador.cargo} 
                         imagem={colaborador.imagem} 
